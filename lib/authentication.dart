a5537a16e1b90f36ccb4a3029e2bf5ad8725882e
Lewis Clark <me@lewisclark.dev>
2024-02-08 11:19:21 +0000

    Add google_sign_in package

---
+++ lib/authentication.dart
@@ -0,0 +1,5 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+const List<String> scopes = <String>[
+  "email",
+];
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import "dart:async";
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import "authentication.dart";
+
 void main() {
   runApp(const MyApp());
 }
