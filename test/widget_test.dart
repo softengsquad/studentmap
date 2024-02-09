5d3115843f39f6d402a0ed65d9bda1698e2f5fdd
Ecstatik <johankool2003@hotmail.co.uk>
2024-02-09 18:47:45 +0000

    Removed unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:studentmap/main.dart';
 
 void main() {
   testWidgets('Test test', (WidgetTester tester) async {
