fcf10c1d7804b4035fb635fe4734de257f68d2b3
Lewis Clark <me@lewisclark.dev>
2024-02-13 15:42:01 +0000

    Create GoogleSignIn object

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -2,4 +2,13 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 const List<String> scopes = <String>[
   "email",
+  "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/calendar.events.owned.readonly",
 ];
+
+GoogleSignIn getGoogleSignIn() {
+  return GoogleSignIn(
+    clientId: "662521222417-ki0ipll3nnsgsf0eeh82forng7ohognc.apps.googleusercontent.com",
+    scopes: scopes,
+  );
+}
