1f5194eab8c07682ecc410089e3e938378466f66
Lewis Clark <me@lewisclark.dev>
2024-02-16 13:43:47 +0000

    Add documentation for const auth variables

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -1,5 +1,11 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
+/// The OAuth 2.0 Client ID given by Google Cloud.
+const String clientId = "662521222417-ki0ipll3nnsgsf0eeh82forng7ohognc.apps.googleusercontent.com";
+
+/// The list of scopes that will be granted to the application
+/// once the user authenticates with Google. This must align with
+/// the scopes defined in the OAuth settings in the Google Cloud API Console.
 const List<String> scopes = <String>[
   "email",
   "https://www.googleapis.com/auth/calendar.readonly",
@@ -8,7 +14,7 @@ const List<String> scopes = <String>[
 
 GoogleSignIn getGoogleSignIn() {
   return GoogleSignIn(
-    clientId: "662521222417-ki0ipll3nnsgsf0eeh82forng7ohognc.apps.googleusercontent.com",
+    clientId: clientId,
     scopes: scopes,
   );
 }
