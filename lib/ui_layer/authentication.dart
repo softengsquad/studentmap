c8fed53c6758a0ed679c04dcad4adb905c9d113d
Lewis Clark <me@lewisclark.dev>
2024-02-17 13:31:52 +0000

    Add trySignIn()

--- lib/ui_layer/authentication.dart
+++ lib/ui_layer/authentication.dart
@@ -1,6 +1,18 @@
-import 'package:google_sign_in/google_sign_in.dart';
+import "package:google_sign_in/google_sign_in.dart";
 
 import "/domain_layer/authenticator.dart";
+
+/// Opens the Google sign-in widget and attempts to authenticate
+/// the user.
+Future<GoogleSignInAccount?> trySignIn() async {
+  GoogleSignIn signIn = getGoogleSignIn();
+
+  try {
+    return signIn.signIn();
+  } catch (error) {
+    return null;
+  }
+}
 
 /// Opens the Google sign-in widget using the predefined client id and
 /// access scopes.
