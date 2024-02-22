9e4ffc16d01ee503ad052675a36ba792f53cb705
Lewis Clark <me@lewisclark.dev>
2024-02-22 11:31:24 +0000

    Update OAuth client id

--- lib/data_layer/google_oauth_api.dart
+++ lib/data_layer/google_oauth_api.dart
@@ -1,5 +1,5 @@
 /// The OAuth 2.0 Client ID given by Google Cloud.
-const String clientId = "662521222417-t6gm7m4ccb13qgssef9be2l8r7tdkvm6.apps.googleusercontent.com";
+const String clientId = "354034818654-ngj5kfhaq8n4j4ou3ok265t0q37c4a8h.apps.googleusercontent.com";
 
 /// The list of scopes that will be granted to the application
 /// once the user authenticates with Google. This must align with
