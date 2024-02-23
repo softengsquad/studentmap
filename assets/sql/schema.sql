aa2d4190eb72be56517cce6836282c7151131b03
Lewis Clark <me@lewisclark.dev>
2024-02-23 17:15:17 +0000

    Add basic `building` table in schema.sql

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,3 +1,7 @@
-CREATE TABLE test(
-    id INT,
+CREATE TABLE IF NOT EXISTS building(
+    building_id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    longitude DECIMAL NOT NULL,
+    latitude DECIMAL NOT NULL,
+    favourite INTEGER (CHECK favourite == 0 OR favourite == 1)
 );
