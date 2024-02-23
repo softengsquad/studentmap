9fb7d8eddf78f0eae761f1153725fbdb14630495
Lewis Clark <me@lewisclark.dev>
2024-02-23 20:47:28 +0000

    Add database test

---
+++ test/data_layer/database_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart' show databaseFactory, databaseFactoryFfi;
+import 'package:studentmap/data_layer/database.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  databaseFactory = databaseFactoryFfi;
+
+  group("Database tests", () {
+    test("Database contains tables after initialization", () async {
+      var db = await Database.open();
+
+      assert(!await db.isDatabaseEmpty());
+    });
+  });
+}
