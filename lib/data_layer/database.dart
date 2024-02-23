3d137b22077aea017ada20cf0cd9299afffb5dd0
Lewis Clark <me@lewisclark.dev>
2024-02-23 16:58:14 +0000

    Add some documentation for Database class

--- lib/data_layer/database.dart
+++ lib/data_layer/database.dart
@@ -1,15 +1,16 @@
 import "package:sqflite/sqflite.dart" as sqflite;
 
+/// Describes a local database used to store various information
+/// pertraining to buildings, bus routes, favourites, etc.
 class Database {
     final sqflite.Database _internalDb;
 
     Database(this._internalDb);
 
-    Future<Database> init() async {
-        return Database(await _initDb());
-    }
+    /// Opens a connection to the local database.
+    Future<Database> open() async {
+        var internalDb = await sqflite.openDatabase("studentmap.db");
 
-    Future<sqflite.Database> _initDb() async {
-        return sqflite.openDatabase("studentmap.db");
+        return Database(internalDb);
     }
 }
