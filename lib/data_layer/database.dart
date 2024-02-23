37f42c7260cb9253a8a3ac8889da4711bc6b3f67
Lewis Clark <me@lewisclark.dev>
2024-02-23 18:48:02 +0000

    Populate databsae

--- lib/data_layer/database.dart
+++ lib/data_layer/database.dart
@@ -11,12 +11,43 @@ class Database {
     /// Opens a connection to the local database.
     static Future<Database> open() async {
         var internalDb = await sqflite.openDatabase("studentmap.db");
-        internalDb.execute(await _getSchemaSql());
 
-        return Database(internalDb);
+        var db = Database(internalDb);
+        await db._init();
+
+        return db;
     }
 
-    static Future<String> _getSchemaSql() async {
+    Future<void> _init() async {
+        if (await isDatabaseEmpty()) {
+            _internalDb.execute(await _getSchemaSql());
+            _internalDb.execute(await _getPopulateSql());
+        }
+    }
+
+    Future<String> _getSchemaSql() async {
         return await rootBundle.loadString("assets/sql/schema.sql");
+    }
+
+    Future<String> _getPopulateSql() async {
+        return await rootBundle.loadString("assets/sql/populate.sql");
+    }
+
+    /// Returns the number of user tables in the database. 
+    /// Does not include internal/system tables.
+    Future<int> getTableCount() async {
+        var res = await _internalDb.query(
+            "sqlite_master",
+            columns: ["count(*)"],
+            where: "type = 'table' AND name != 'android_metadata' AND name != 'sqlite_sequence'"
+        );
+
+        return res.first["count(*)"] as int;
+    }
+
+    /// Returns true if the database contains 0 user tables.
+    /// See getTableCount()
+    Future<bool> isDatabaseEmpty() async {
+        return await getTableCount() <= 0;
     }
 }
