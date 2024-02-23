740c6417d4e203781ff8941e3fab536d3011f6d7
Lewis Clark <me@lewisclark.dev>
2024-02-23 17:08:16 +0000

    Execute database schema on initialization

--- lib/data_layer/database.dart
+++ lib/data_layer/database.dart
@@ -1,3 +1,4 @@
+import "package:flutter/services.dart" show rootBundle;
 import "package:sqflite/sqflite.dart" as sqflite;
 
 /// Describes a local database used to store various information
@@ -10,7 +11,12 @@ class Database {
     /// Opens a connection to the local database.
     Future<Database> open() async {
         var internalDb = await sqflite.openDatabase("studentmap.db");
+        internalDb.execute(await _getSchemaSql());
 
         return Database(internalDb);
+    }
+
+    static Future<String> _getSchemaSql() async {
+        return await rootBundle.loadString("assets/sql/schema.sql");
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import "/ui_layer/authentication.dart";
+import "/data_layer/database.dart";
 
 void main() {
   runApp(const MyApp());
