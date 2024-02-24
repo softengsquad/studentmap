87ceebeb8ed43695bbe448c3393861ffb8066a68
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2024-02-24 13:58:18 +0000

    Created Map Info class template

---
+++ lib/domain_layer/building_manager/maps_info.dart
@@ -0,0 +1,19 @@
+/// This is a class that will store information about where a building is
+/// located on Google Maps.
+class MapsInfo{
+
+  final double longitude;
+  final double latitude;
+
+
+  MapsInfo({
+    required this.latitude,
+    required this.longitude
+  });
+
+
+  getLocation(){
+    
+  }
+
+}
