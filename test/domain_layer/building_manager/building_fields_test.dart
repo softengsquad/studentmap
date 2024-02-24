765e312a9ca6286afdd6b540162567932c0148fc
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2024-02-24 14:17:26 +0000

    Updated field unit tests as well as unit testing structure

--- test/domain_layer/building_manager/building_fields_test.dart
+++ test/domain_layer/building_manager/building_fields_test.dart
@@ -13,6 +13,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -22,7 +24,9 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect(1, testObj.id);
@@ -37,6 +41,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -46,12 +52,16 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect("Richmond", testObj.name);
     });
 
+
+    // Test for initializing addressLine1
     test("Address Line 1 initializes to 'Portland St'", () {
       // Arrange
       int id = 1;
@@ -61,6 +71,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -70,12 +82,15 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect("Portland St", testObj.addressLine1);
     });
 
+// Test for initializing addressLine2
     test("Address Line 2 initializes to 'Portsmouth'", () {
       // Arrange
       int id = 1;
@@ -85,6 +100,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -94,12 +111,15 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect("Portsmouth", testObj.addressLine2);
     });
 
+// Test for initializing postcode
     test("Postcode initializes to 'PO1 3DE'", () {
       // Arrange
       int id = 1;
@@ -109,6 +129,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -118,12 +140,15 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect("PO1 3DE", testObj.postcode);
     });
 
+// Test for initializing type
     test("Type initializes to 'uni'", () {
       // Arrange
       int id = 1;
@@ -133,6 +158,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -142,12 +169,15 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect("uni", testObj.type);
     });
 
+// Test for initializing favourited
     test("Favourited initializes to 'false'", () {
       // Arrange
       int id = 1;
@@ -157,6 +187,8 @@ void main() {
       String postcode = "PO1 3DE";
       String type = "uni";
       bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
 
       // Act
       Building testObj = Building(
@@ -166,10 +198,70 @@ void main() {
           addressLine2: addressLine2,
           postcode: postcode,
           favourited: favourited,
-          type: type);
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
 
       // Assert
       expect(false, testObj.favourited);
+    });
+
+// Test for initializing longitude
+    test("Longitude initializes to '50.7981'", () {
+      // Arrange
+      int id = 1;
+      String name = "Richmond";
+      String addressLine1 = "Portland St";
+      String addressLine2 = "Portsmouth";
+      String postcode = "PO1 3DE";
+      String type = "uni";
+      bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
+
+      // Act
+      Building testObj = Building(
+          id: id,
+          name: name,
+          addressLine1: addressLine1,
+          addressLine2: addressLine2,
+          postcode: postcode,
+          favourited: favourited,
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
+
+      // Assert
+      expect(50.7981, testObj.longitude);
+    });
+
+// Test for initializing latitude
+    test("Latitude initializes to '1.0995'", () {
+      // Arrange
+      int id = 1;
+      String name = "Richmond";
+      String addressLine1 = "Portland St";
+      String addressLine2 = "Portsmouth";
+      String postcode = "PO1 3DE";
+      String type = "uni";
+      bool favourited = false;
+      double longitude = 50.7981;
+      double latitude = 1.0995;
+
+      // Act
+      Building testObj = Building(
+          id: id,
+          name: name,
+          addressLine1: addressLine1,
+          addressLine2: addressLine2,
+          postcode: postcode,
+          favourited: favourited,
+          type: type,
+          longitude: longitude,
+          latitude: latitude);
+
+      // Assert
+      expect(1.0995, testObj.latitude);
     });
   });
 }
