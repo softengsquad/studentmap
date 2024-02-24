af13a50d7ce34c7eec23758fa2d954886b4c0b3e
Lewis Clark <me@lewisclark.dev>
2024-02-24 09:06:54 +0000

    Expand schema for `building` table

--- assets/sql/populate.sql
+++ assets/sql/populate.sql
@@ -1,2 +1,22 @@
-INSERT INTO building (name, longitude, latitude, favourite)
-VALUES ('Richmond Building', 50.7981248012259, -1.0994962047794432, FALSE);
+INSERT INTO building
+    (
+        name,
+        addressline1,
+        addressline2,
+        postcode,
+        type,
+        longitude,
+        latitude,
+        favourited
+    )
+VALUES
+    (
+    'Richmond Building',
+    'Portland Street',
+    NULL,
+    'PO13DE',
+    'UNI',
+    50.7981248012259,
+    -1.0994962047794432,
+    FALSE
+);
