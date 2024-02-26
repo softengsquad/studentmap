ce80da71a5fe50cf6ceb7fb32eebd951ffc89ea6
Lewis Clark <me@lewisclark.dev>
2024-02-26 14:43:11 +0000

    Add Lion Gate and Burnby buildings

--- assets/sql/populate.sql
+++ assets/sql/populate.sql
@@ -11,12 +11,32 @@ INSERT INTO building
     )
 VALUES
     (
-    'Richmond Building',
-    'Portland Street',
-    NULL,
-    'PO13DE',
-    'uni',
-    50.7981248012259,
-    -1.0994962047794432,
-    FALSE
-);
+        'Richmond Building',
+        'Portland Street',
+        NULL,
+        'PO13DE',
+        'uni',
+        50.79806904953112,
+        -1.0996521134195063,
+        FALSE
+    ),
+    (
+        'Lion Gate Building',
+        'Lion Terrace',
+        NULL,
+        'PO13HF',
+        'uni',
+        50.79898426184816,
+        -1.0983448071633066,
+        FALSE
+    ),
+    (
+        'Burnaby Building',
+        'Burnaby Road',
+        NULL,
+        'PO13QL',
+        'uni',
+        50.797989465502134,
+        -1.0979735294992936,
+        FALSE
+    );
