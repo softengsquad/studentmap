0d3b9c4152aba42788bc49de2e84443ff4f87de1
Johan Kool <94987086+JohanKool3@users.noreply.github.com>
2024-02-27 12:24:02 +0000

    Fixed convention to follow other unit tests

--- test/data_layer/database_test.dart
+++ test/data_layer/database_test.dart
@@ -1,37 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart' show databaseFactory, databaseFactoryFfi;
+import 'package:sqflite_common_ffi/sqflite_ffi.dart'
+    show databaseFactory, databaseFactoryFfi;
 import 'package:studentmap/data_layer/database.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   databaseFactory = databaseFactoryFfi;
 
-  group("Database", () {
+  group("Database fields |", () {
     test("contains tables after initialization", () async {
       var db = await Database.open();
 
-      assert(!await db.isDatabaseEmpty());
+      expect(true, !await db.isDatabaseEmpty());
     });
   });
 
-  group("Database buildings", () {
-    test("table is not empty", () async {
+  group("Database Methods |", () {
+    test("getAllBuildings is not empty", () async {
       var db = await Database.open();
       var buildings = await db.getAllBuildings();
 
-      assert(buildings.isNotEmpty);
+      expect(true, buildings.isNotEmpty);
     });
 
-    test("first building row is valid", () async {
+    test(
+        "getAllBuildings , first row contains [1, 'Richmond Building', 'uni', FALSE]",
+        () async {
       var db = await Database.open();
       var buildings = await db.getAllBuildings();
 
       var b1 = buildings.first;
 
-      assert(b1.id == 1);
-      assert(b1.name == "Richmond Building");
-      assert(b1.type == "uni");
-      assert(!b1.favourited);
+      expect(1, b1.id);
+      expect("Richmond Building", b1.name);
+      expect("uni", b1.type);
+      expect(false, b1.favourited);
     });
   });
 }
