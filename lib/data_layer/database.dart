6adcdb320eac5d006920c2d32f21bb17415dabd7
Lewis Clark <me@lewisclark.dev>
2024-02-29 18:15:44 +0000

    Use parameter `fave` in update

--- lib/data_layer/database.dart
+++ lib/data_layer/database.dart
@@ -108,7 +108,7 @@ class Database {
   }
 
   Future<void> updateFavouriteStatus(int id, bool fave) async {
-    await _internalDb.update("building", {"{favourited": 1},
+    await _internalDb.update("building", {"favourited": fave},
         where: "building_id = $id");
   }
 }
