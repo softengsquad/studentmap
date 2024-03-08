14326ed5a46812754d58f839c1b16f85ec288d6f
Lewis Clark <me@lewisclark.dev>
2024-03-08 12:09:36 +0000

    Add all buildings in north-west quarter

--- assets/sql/populate.sql
+++ assets/sql/populate.sql
@@ -13,7 +13,7 @@ VALUES
     (
         'Richmond Building',
         'Portland Street',
-        NULL,
+        'Portsmouth',
         'PO13DE',
         'uni',
         50.79806904953112,
@@ -23,7 +23,7 @@ VALUES
     (
         'Lion Gate Building',
         'Lion Terrace',
-        NULL,
+        'Portsmouth',
         'PO13HF',
         'uni',
         50.79898426184816,
@@ -33,10 +33,80 @@ VALUES
     (
         'Burnaby Building',
         'Burnaby Road',
-        NULL,
+        'Portsmouth',
         'PO13QL',
         'uni',
         50.797989465502134,
         -1.0979735294992936,
+        FALSE
+    ),
+    (
+        'Burnaby Terrace',
+        'Burnaby Road',
+        'Portsmouth',
+        'PO13AE',
+        'uni',
+        50.797242412512105,
+        -1.0985521998926695,
+        FALSE
+    ),
+    (
+        'Buckingham Building',
+        'Lion Terrace',
+        'Portsmouth',
+        'PO13HE',
+        'uni',
+        50.798431756426744,
+        -1.0985311315926916,
+        FALSE
+    ),
+    (
+        'Mildam Building',
+        'Burnaby Road',
+        'Portsmouth',
+        'PO13AS',
+        'uni',
+        50.797358022417654,
+        -1.0991927254536318,
+        FALSE
+    ),
+    (
+        'Dennis Sciama Building',
+        'Burnaby Road',
+        'Portsmouth',
+        'PO13FX',
+        'uni',
+        50.79770219300847,
+        -1.0983113172087318,
+        FALSE
+    ),
+    (
+        'Anglesea Building',
+        'Anglesea Road',
+        'Portsmouth',
+        'PO13DJ',
+        'uni',
+        50.797750175564126,
+        -1.0964797235811874,
+        FALSE
+    ),
+    (
+        'Future Technology Centre',
+        'Portland Street',
+        'Portsmouth',
+        'PO13HE',
+        'uni',
+        50.798717608832035,
+        -1.099322245983245,
+        FALSE
+    ),
+    (
+        'Portland Building',
+        'Portland Street',
+        'Portsmouth',
+        'PO13AH',
+        'uni',
+        50.79846404980428,
+        -1.0994614493743617,
         FALSE
     );
